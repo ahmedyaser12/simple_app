642d654947d6bb5abe666cbd1d95ca9a3e6326a6
AhmedYasserabdelraziq <ahmedyaserramadan456@gmail.com>
2024-04-23 09:38:31 +0200

    refactor some details in UI and some files

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,5 @@
+class AssetsManager {
+  static const String backgroundLogin = 'assets/images/log in.png';
+  static const String backgroundHome = 'assets/images/background.png';
+  static const String svgPath = 'assets/svgs/';
+}
